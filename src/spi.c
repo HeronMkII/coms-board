f5f956d732ecc7e28240dbd8b792806424116b53
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2017-08-21 18:20:08 -0400

    Formatting and switch to fixed-width data types

--- src/spi.c
+++ src/spi.c
@@ -4,6 +4,7 @@
 #define CLK PB7
 #define MISO PB0
 #define MOSI PB1
+#define SS PD3
 
 void init_cs(int pin, PORT ddr) {
     *ddr |= _BV(pin);
@@ -19,16 +20,18 @@ void set_cs_high(int pin, PORT port) {
 
 // Before SPI is enables, PPRSPI must be 0
 void init_spi(void) {
-    // make CLK and MOSI pins output
+    // make CLK, MOSI, and SS pins output
     DDRB |= _BV(CLK) | _BV(MOSI);
+    DDRD |= _BV(SS);
     // enable SPI, set mode to master, set SCK freq to f_io/64
     SPCR |= _BV(SPE) | _BV(MSTR) | _BV(SPR1);
 }
 
-char send_spi(char cmd) {
+uint8_t send_spi(uint8_t cmd) {
     // TODO: the slave device data mode must match the configured data mode;
     // this often works with the defaults, not not always.
+    int TIMEOUT = 65535;
     SPDR = cmd;
-    while (!(SPSR & _BV(SPIF)));
+    while (!(SPSR & _BV(SPIF)) && TIMEOUT--);
     return SPDR;
 }
